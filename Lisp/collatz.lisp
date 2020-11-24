8a829c23af33ef3f91e160d1bfb4429a0cd1b678
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2020-11-24 02:21:45 -0500

    Updates to Lisp

--- Lisp/collatz.lisp
+++ Lisp/collatz.lisp
@@ -4,34 +4,37 @@
 (defvar comp)
 (defvar csteps 0) ;;Defining the counter for steps as zero
 (defvar maxVal) ;;Highest integer 
-(setq maxVal 500)
+(setq maxVal 10)
 (defvar comp) ;;Integer  
 (defvar i)
-(defvar n)
 ;;Creates an ordered pair that can hold both position and NumberSteps of the sequence
 (defstruct CollatzTuple
 	(pos 0)   ;;Defines the position (magnitude of the number)
 	(nstep 0) ;;Sequence value for each magnitude
 )
 
-
+;;Creation of the array
+;;(setq SArray (make-array '(50)))
 
 ;;Function to calculate collatz
-(defun collatz( n csteps ) 
-    (loop while (/= n 1) do           ;;While n doesnt eqaul 1
-        (if (= (mod n 2) 1)         ;;If odd
-            (setq n (+ (* 3 pos) 1))    
-            (setq n (/ n 2))          ;;If even
+(defun collatz( pos csteps ) 
+    (loop while (/= pos 1) do           ;;While n doesnt eqaul 1
+        (if (= (mod pos 2) 1)         ;;If odd
+            (setq pos (+ (* 3 pos) 1))    
+            (setq pos (/ pos 2))          ;;If even
         )
         (setq csteps (+ csteps 1))    ;;Increment csteps
     )
     (return-from collatz csteps) ;;;Returns the number of steps that the sequence works
 )
-(setf n 8)
-(write(collatz csteps))
-;;Major Calculation Loop
-; (setf i 2)
-; (loop
-;     (setq csteps (collatz i csteps))
-;     (write csteps)
-; )
+
+(setf i 2)
+(loop
+	(setf csteps (collatz i csteps))
+    (write i)
+     
+	(write csteps)
+    (princ "   ")
+	(setf i (+ i 1))
+	(when (> i 50) (return))
+)
