961a09af9e4c5bca430470e82926f686387033fc
Colton R. Steinbeck <steinbeck_cr@cobra.cs.mercer.edu>
2020-11-24 01:52:40 -0500

    Collatz Sequence runs for a Given Value

--- Lisp/collatz.lisp
+++ Lisp/collatz.lisp
@@ -1,42 +1,37 @@
 #!/usr/bin/sbcl --script
-(defvar newNum)
-(defvar CStepArray)
-(defvar size)
-(defvar csteps 0)
-(defvar steps 0)
-(defvar maxVal)
+(defvar pos)
+(defvar nsteps)
 (defvar comp)
-(defvar n)
+(defvar csteps 0) ;;Defining the counter for steps as zero
+(defvar maxVal) ;;Highest integer 
+(setq maxVal 500)
+(defvar comp) ;;Integer  
 (defvar i)
-(defvar j)
-(defvar StepArray)
-
-;;Creates an ordered pair that can hold both position and size of the sequence
+(defvar n)
+;;Creates an ordered pair that can hold both position and NumberSteps of the sequence
 (defstruct CollatzTuple
-	(pos 0)
-	(NumberSteps 0)
+	(pos 0)   ;;Defines the position (magnitude of the number)
+	(nstep 0) ;;Sequence value for each magnitude
 )
-(setf StepArray(make-array 10))
 
-(defun collatz(n csteps) ;;Function to calculate collatz
+
+
+;;Function to calculate collatz
+(defun collatz( n csteps ) 
     (loop while (/= n 1) do           ;;While n doesnt eqaul 1
         (if (= (mod n 2) 1)         ;;If odd
-            (setq n (+ (* 3 n) 1))    
+            (setq n (+ (* 3 pos) 1))    
             (setq n (/ n 2))          ;;If even
         )
         (setq csteps (+ csteps 1))    ;;Increment csteps
     )
     (return-from collatz csteps) ;;;Returns the number of steps that the sequence works
 )
-
-
-
-(setf i 2)
-(loop
-	(setf steps (collatz i steps)) ;;Steps that it takes to reach the collatz sequence
-	(setf (aref StepArray (i)) (make-CollatzTuple :pos i :NumberSteps steps)) ;; Steps the position and steps of the tuple to what the c
-	(setf i (+ i 1)) ;;Increments i
-	(when (> i 10) (return)) ;; While the value is less than 10
-)
-
-(set i 1) ;; Going to loop from thye
+(setf n 8)
+(write(collatz csteps))
+;;Major Calculation Loop
+; (setf i 2)
+; (loop
+;     (setq csteps (collatz i csteps))
+;     (write csteps)
+; )
