130903807557d76be798360a345860ca2ff18f56
Colton Steinbeck <steinbeck.colton@gmail.com>
2024-07-06 17:53:49 -0400

    Zig Collatz WIP

--- Zig/collatz.zig
+++ Zig/collatz.zig
@@ -1,16 +1,20 @@
 const std = @import("std");
 
-fn collatz(n: u64) u64 {
+fn collatz(n: u64, counter: u64) u64 {
+    var localCounter = counter + 1;
+    std.debug.print("{}: {}\n", .{ counter, n });
     if (n == 1) {
-        return 1;
-    } else if (n % 2 == 0) {
-        return collatz(n / 2);
+        return localCounter;
+    } else if (n % 2 == 0 and n <= 5_000_000_000) {
+        return collatz(n / 2, localCounter);
     } else {
-        return collatz(3 * n + 1);
+        return collatz(3 * n + 1, localCounter);
     }
+    //todo: store all n in array of type u64, use a sorting algorithm to get 10 lowest
 }
 
 pub fn main() void {
-    const result = collatz(25);
-    std.debug.print("Result: {}\n", .{result});
+    const startValue: u64 = 5_000_000_000;
+    const result = collatz(startValue, 0); // Start with 0 as the initial counter value
+    std.debug.print("Steps for {}: {}\n", .{ startValue, result });
 }
